49c74b30d510732bf7aac15f13d9ccd8c9106946
Thomas van Oort <t.van.oort@amsterdam.nl>
2023-06-01 13:11:01 +0200

    Make a redirect with direct logout working

--- keycloak_oidc/utils.py
+++ keycloak_oidc/utils.py
@@ -11,6 +11,8 @@ def generate_username(email):
 
 
 def oidc_op_logout(request):
+    session = request.session
+
     oidc_op_logout_endpoint = settings.OIDC_OP_LOGOUT_ENDPOINT
     redirect_url = request.build_absolute_uri(getattr(settings, 'LOGOUT_REDIRECT_URL', '/'))
-    return '{}?redirect_uri={}'.format(oidc_op_logout_endpoint, redirect_url)
+    return '{}?post_logout_redirect_uri={}&id_token_hint={}'.format(oidc_op_logout_endpoint, redirect_url, session["oidc_id_token"])
